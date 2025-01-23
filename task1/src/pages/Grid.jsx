56e424e43e50a9881d380651162cd9445b2cc7c1
WEBDEV-Rajat <162951475+WEBDEV-Rajat@users.noreply.github.com>
2025-01-23 08:55:36 +0530

    final commit

--- task1/src/pages/Grid.jsx
+++ task1/src/pages/Grid.jsx
@@ -9,28 +9,39 @@ const Grid = () => {
     '#00407A', '#00407A', '#00407A', '#00407A',
     '#00407A', '#00407A', '#00407A', '#00407A', '#7A0000',
   ]);
+  const [clickOrder, setClickOrder] = useState([]);
   const [colorGrid, setColorGrid] = useState(false);
 
-  const handleColor = (index) => {
-    if (index === color.length - 1) {
-      setColor([
-        '#00407A', '#00407A', '#00407A', '#00407A',
-        '#00407A', '#00407A', '#00407A', '#00407A', '#7A0000',
-      ]);
-      return;
+  const handleSquareClick = (index) => {
+    const newColors = [...color];
+    newColors[index] = '#7A0000';
+    setColor(newColors);
+    setClickOrder([...clickOrder, index]);
+
+    if (index === 8) {
+      setTimeout(() => {
+        const resetColors = [...color];
+        clickOrder.forEach((i, idx) => {
+          setTimeout(() => {
+            if(i === 8) {
+              resetColors[i] = '#7A0000';
+            }
+            resetColors[i] = '#00407A';
+            setColor([...resetColors]);
+          }, idx * 100);
+        });
+        setClickOrder([]);
+      }, 500);
     }
-    const newColor = [...color];
-    newColor[index] = newColor[index] === '#00407A' ? '#7A0000' : '#00407A';
-    setColor(newColor);
   };
 
   return (
     <div>
       <Navbar />
       <div className="m-4 md:m-16 mt-12 items-center">
-        {/* Grid Container */}
+        
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-          {/* 1st Box (Gradient) */}
+          
           {gradient ? (
             <div className="bg-gradient-to-br from-[#3300FF] to-[#FA00FF] h-[385px] rounded-xl border border-[#D9D9D9] flex justify-center items-center lg:col-span-1 md:col-span-2 order-3 sm:order-none md:order-1"></div>
           ) : (
@@ -44,7 +55,7 @@ const Grid = () => {
             </div>
           )}
 
-          {/* 2nd Box (Logo) */}
+          
           {logo ? (
             <div className="h-[385px] rounded-xl border border-[#D9D9D9] flex justify-center items-center order-2 sm:order-none md:order-2 lg:col-span-1">
               <img src={img1} alt="logo" />
@@ -60,7 +71,7 @@ const Grid = () => {
             </div>
           )}
 
-          {/* 3rd Box (Color Grid) */}
+          
           {colorGrid ? (
             <div className="h-[385px] rounded-xl bg-[#D9D9D9] border border-[#D9D9D9] flex justify-center items-center order-1 sm:order-none md:order-3 lg:col-span-1">
               <div className="h-[300px] border w-3/4 rounded-lg grid grid-cols-3 gap-2">
@@ -69,7 +80,7 @@ const Grid = () => {
                     key={index}
                     style={{ backgroundColor: c }}
                     className="cursor-pointer rounded-lg"
-                    onClick={() => handleColor(index)}
+                    onClick={() => handleSquareClick(index)}
                   ></div>
                 ))}
               </div>
